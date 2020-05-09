2d645d314ef448d134e0beda0e78424fd780b215
Masayuk iMaekawa <maepon@gmail.com>
2020-05-09 13:09:49 +0900

    prop-types

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function App() {
-  return (
-      <React.Fragment>
-        <label htmlFor="bar">Bar</label>
-        <input id="bar" type="text" onChange={() => {console.log('changed')}}/>
-        <Cat/>
-        <Cat/>
-        <Cat/>
-        <Cat/>
-      </React.Fragment>
-  );
+    const profiles = [
+        {name: '太郎',age: 10},
+        {name: '花子',age: 8},
+        {name: '名無し', age: 12},
+        ];
+
+    return (
+        <React.Fragment>
+            {
+                profiles.map(
+                    (profile, index) => {
+                        return <User name={profile.name} age={profile.age} key={index}/>
+                    }
+                )
+            }
+        </React.Fragment>
+    );
 }
 
-const Cat = () => {
+const User = (props) => {
     return (
-        <div>にゃー</div>
+        <div>Hi! {props.name} 年齢 {props.age}歳！</div>
     )
 }
+
+User.propTypes = {
+    name: PropTypes.string,
+    age: PropTypes.number.isRequired
+};
 
 export default App;
