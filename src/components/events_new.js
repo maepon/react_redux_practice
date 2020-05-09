6e39540673bafb59467fb38bb1a4bc988b5e3044
Masayuk iMaekawa <maepon@gmail.com>
2020-05-09 18:10:22 +0900

    submit カスタマイズ

--- src/components/events_new.js
+++ src/components/events_new.js
@@ -29,7 +29,7 @@ class EventsNew extends Component {
     }
 
     render() {
-        const {handleSubmit} = this.props;
+        const {handleSubmit, pristine, submitting} = this.props;
         return (
             <React.Fragment>
                 <form onSubmit={handleSubmit(this.onSubmit)}>
@@ -41,7 +41,7 @@ class EventsNew extends Component {
                     </div>
 
                     <div>
-                        <input type="submit" value="Submit" disabled={false}/>
+                        <input type="submit" value="Submit" disabled={pristine || submitting}/>
                         <Link to='/'>Cancel</Link>
                     </div>
                 </form>
