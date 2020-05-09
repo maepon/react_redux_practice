84e36562da02dbd513acf07cba302b615bc772a4
Masayuk iMaekawa <maepon@gmail.com>
2020-05-09 12:34:48 +0900

    コンポーネント

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
 
 function App() {
-  const greeting = 'Hi!!!';
-  const dom = <h1 className="foo">{greeting}</h1>;
   return (
       <React.Fragment>
         <label htmlFor="bar">Bar</label>
         <input id="bar" type="text" onChange={() => {console.log('changed')}}/>
+        <Cat/>
+        <Cat/>
+        <Cat/>
+        <Cat/>
       </React.Fragment>
   );
+}
+
+const Cat = () => {
+    return (
+        <div>にゃー</div>
+    )
 }
 
 export default App;
