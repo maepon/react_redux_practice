b266b84c1dd9207e55e068d936ec2c075538fe0c
Masayuk iMaekawa <maepon@gmail.com>
2020-05-09 16:48:29 +0900

    event list 作成

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from "redux";
-import { Provider } from 'react-redux';
+import {createStore, applyMiddleware} from "redux";
+import {Provider} from 'react-redux';
+import thunk from "redux-thunk";
 
 import './index.css';
 import reducer from './reducers'
 
-import App from './components/App';
+import EventsIndex from './components/events_index';
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(thunk));
 
 ReactDOM.render(
     <Provider store={store}>
         <React.StrictMode>
-            <App />
+            <EventsIndex/>
         </React.StrictMode>
     </Provider>,
-  document.getElementById('root')
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
